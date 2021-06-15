816519e601eb9dd6d89fa65840784fce96c6601a
Tristan Gibbs <69325323+gibbsdigital@users.noreply.github.com>
2021-06-14 20:54:55 -0700

    register plugins, utils, and config in .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,123 @@
+require('dotenv').config()
+
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const markdownIt = require("markdown-it");
+const markdownItAttrs = require('markdown-it-attrs');
+const filters = require('./utils/filters.js');
+//const transforms = require('./utils/transforms.js');
+const shortcodes = require('./utils/shortcodes.js');
+
+const IS_PRODUCTION = process.env.ELEVENTY_ENV === 'production'
+
+
+
+
 
 
 // ELEVENTY CONFIG
 module.exports = function (eleventyConfig) {
 
+   
+
     // PLUGINS
+    eleventyConfig.addPlugin( pluginRss );
+    eleventyConfig.addPlugin( pluginSyntaxHighlight );
+
+
 
     // FILTERS
+    eleventyConfig.addFilter("dateFriendly", filters.dateFriendly );
+    eleventyConfig.addFilter("dateYMD", filters.dateYMD );
+    eleventyConfig.addFilter("dateISO", filters.dateISO );
+    eleventyConfig.addFilter("readTime", filters.readTime );
+    eleventyConfig.addFilter("excludePost", filters.excludePost );
+    eleventyConfig.addFilter("slice", filters.slice );
 
+
+    /**
+     * TODO: set up minifyHTML for use with svg-defs-sprite embed. Fix sprite, up arrow
+     */
     // TRANSFORMS
+    /* eleventyConfig.addTransform('minifyCSS', transforms.minifyCSS );
+    eleventyConfig.addTransform('minifyHTML', transforms.minifyHTML );
+    eleventyConfig.addTransform('minifyJS', transforms.minifyJS );
+ */
 
     // SHORTCODES
+    eleventyConfig.addShortcode("year", shortcodes.year );
+    eleventyConfig.addNunjucksAsyncShortcode("image", shortcodes.eleventyImage);
+
+
+
 
     // COLLECTIONS
+    eleventyConfig.addCollection("posts", (collection) => {
+        return collection
+            .getFilteredByGlob("./src/posts/*.md")
+            .filter((p) => !(p.data.draft && IS_PRODUCTION));
+    });
 
-    // MARKDOWN
+
+    eleventyConfig.addCollection("featuredPosts", (collection) => {
+        return collection
+            .getFilteredByGlob("./src/posts/*.md")
+            .filter((p) => p.data.featured);
+    });
+
+    
+    eleventyConfig.addCollection("projects", (collection) => {
+        return collection
+            .getFilteredByGlob("./src/projects/*.md")
+            .sort((a, b) =>
+                Number(a.data.displayOrder) > Number(b.data.displayOrder)
+                    ? 1
+                    : -1
+            );
+    });
+
+    
+    eleventyConfig.addCollection("featuredProjects", (collection) => {
+        return collection
+            .getFilteredByGlob("./src/projects/*.md")
+            .sort((a, b) =>
+                Number(a.data.displayOrder) > Number(b.data.displayOrder)
+                    ? 1
+                    : -1
+            )
+            .filter((p) => p.data.featured);
+    });
+
+    
+    eleventyConfig.addCollection("tagList", filters.getTagList );
+   
+
+
+
+    /* MARKDOWN IT  - https://markdown-it.github.io/markdown-it/  */
+    const markdownLib = markdownIt({
+        html: true,
+        breaks: true,
+        linkify: true
+    }).use(markdownItAttrs);
+    eleventyConfig.setLibrary('md', markdownLib);
+
+
+
 
     // PASSTHROUGH FILE COPY
+    eleventyConfig.addPassthroughCopy("./src/assets/images/favicons");
+    eleventyConfig.addPassthroughCopy("./src/assets/images/projects");
+    eleventyConfig.addPassthroughCopy("./src/assets/fonts");
     
-    // WATCH TARGET
 
-    // RETURN THE BASE CONFIG
+
+    // WATCH TARGET
+    eleventyConfig.addWatchTarget('./src/assets');
+
+
+
+    // RETURN THE BASE CONFIG OBJECT
     return {
         markdownTemplateEngine: "njk",
         dataTemplateEngine: "njk",
@@ -31,4 +129,4 @@ module.exports = function (eleventyConfig) {
         },
     };
 
-}; // END ELEVENTY CONFIG
+}; // END ELEVENTY eleventyConfig
